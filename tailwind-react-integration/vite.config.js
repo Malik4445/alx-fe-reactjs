5fa6dbae1f7197c402a385139455ec84d3dbf8a3
Malik4445 <abdulmalikenyoneibrahim@gmail.com>
2025-09-26 10:32:06 +0100

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,12 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // Add Tailwind CSS as a plugin for compatibility with some checkers
-    tailwindcss(),
-  ],
+	plugins: [react()],
 })
+
