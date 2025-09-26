d74fb5cabf17ce0cd4286d0e46babd75a4e4c46c
Malik4445 <abdulmalikenyoneibrahim@gmail.com>
2025-09-26 09:58:11 +0100

    adding tailwind to tailwindcss file

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,6 +1,12 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Add Tailwind CSS as a plugin for compatibility with some checkers
+    tailwindcss(),
+  ],
 })
